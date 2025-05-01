29fcd4254685c8d3bd75a2555423676ea2413352
Jvlima22_ <josulima90@gmail.com>
2025-05-01 10:58:56 -0300

    updated crud

--- api.js
+++ api.js
@@ -6,22 +6,3 @@ const api = axios.create({
   timeout: 10000,
 });
 
-export const getMinutas = async () => {
-  try {
-    const response = await api.get('/minuta');
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao obter minutas:', error.message);
-  }
-};
-
-export const createMinuta = async (minutaData) => {
-  try {
-    const response = await api.post('/minuta', minutaData);
-    return response.data;
-  } catch (error) {
-    console.error('Erro ao criar minuta:', error.message);
-  }
-};
-
-// Adicione outras funções CRUD conforme necessário
